0b077f6f274f7c28bf68f5de60e6e948a8d81710
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-03-13 19:58:28 +0200

    New exception

---
+++ CCLibrary/Exceptions/ProfiletNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CCLibrary.Exceptions
+{
+    public class ProfiletNotFoundException : Exception
+    {
+        private const string message = "Профіль не знайдено!";
+        public ProfiletNotFoundException(string message = message) : base(message) { }
+    }
+}
