79de316ef7cde9ab2325ab40ea04f3c633d7bd2c
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-03-13 20:15:45 +0200

    Profile and DailyConsumption classes

---
+++ CCLibrary/User/DailyConsumption.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using CCLibrary.Products;
+
+namespace CCLibrary.User
+{
+    public class DailyConsumption
+    {
+        public Profile LinkedProfile { get; }
+        public DateTime Date { get; }
+        private List<Product> _consumedProducts;
+
+        public DailyConsumption(Profile profile, DateTime date)
+        {
+            LinkedProfile = profile;
+            Date = date;
+            _consumedProducts = new List<Product>();
+        }
+
+        /// <summary>
+        /// Calculate the total amount of consumed calories.
+        /// </summary>
+        public double CalculateCalories()
+        {
+            return _consumedProducts.Select(product => product.GetCalories()).Sum();
+        }
+
+        /// <summary>
+        /// Calculate the total amount of consumed energy.
+        /// </summary>
+        public double CalculateEnergy()
+        {
+            return CalculateCalories() * 4.184;
+        }
+
+        /// <summary>
+        /// Get a copy of a list of consumed products.
+        /// </summary>
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_consumedProducts);
+        }
+
+        /// <summary>
+        /// Add product to consumed list.
+        /// </summary>
+        public void Consume(Product product)
+        {
+            _consumedProducts.Add(product);
+        }
+
+        /// <summary>
+        /// Set a new list of consumed products.
+        /// </summary>
+        public void UpdateConsumed(List<Product> products)
+        {
+            _consumedProducts = new List<Product>(products);
+        }
+    }
+}
---
+++ CCLibrary/User/Profile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using CCLibrary.Exceptions;
+
+namespace CCLibrary.User
+{
+    public class Profile
+    {
+        internal string _login;
+        private string _password;
+
+        public ulong Id { get; set; }
+        public string Name { get; set; } = "N/D";
+        public byte[]? Image { get; set; }
+
+        public DateTime BirthDay { get; set; }
+        protected float _height;
+        protected float _weight;
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Today.Year - BirthDay.Year;
+                if (DateTime.Today < BirthDay.AddYears(age)) age--;
+                return age;
+            }
+        }
+
+        public float HeightInCm
+        {
+            get { return _height; }
+            set
+            {
+                if (value <= 0)
+                    throw new ValueOutOfRangeException("Значення повинно бути додатнім!");
+                _height = value;
+            }
+        }
+
+        public float WeightInKg
+        {
+            get { return _weight; }
+            set
+            {
+                if (value <= 0)
+                    throw new ValueOutOfRangeException("Значення повинно бути додатнім!");
+                _weight = value;
+            }
+        }
+
+        public Profile(string login, string password)
+        {
+            _login = login;
+            _password = password;
+        }
+
+        /// <summary>
+        /// Перевірити пароль на дійсність.
+        /// </summary>
+        public bool CheckPassword(string password)
+        {
+            return _password.Equals(password, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Оновити поточний пароль.
+        /// </summary>
+        /// <exception cref="WrongPasswordException"></exception>
+        public void UpdatePassword(string oldPassword, string newPassword)
+        {
+            if (CheckPassword(oldPassword))
+                _password = newPassword;
+            else
+                throw new WrongPasswordException();
+        }
+    }
+}
