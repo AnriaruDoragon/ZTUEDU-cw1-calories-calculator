ef09d61a2746881ff900c828f9db212f077deeb0
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-03-13 20:16:17 +0200

    Products and Profiles DB contexts

---
+++ CCLibrary/Data/ProductContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using CCLibrary.Products;
+using CCLibrary.Exceptions;
+
+namespace CCLibrary.Data
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(Data.Database._dbSource);
+
+        /// <summary>
+        /// Get the product by ID.
+        /// </summary>
+        public Product? GetProduct(ulong id)
+        {
+            return Products.FirstOrDefault(product => product.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Add a product to the database.
+        /// </summary>
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+            SaveChanges();
+        }
+
+        /// <summary>
+        /// Remove product from the database.
+        /// </summary>
+        /// <exception cref="ProductNotFoundException"></exception>
+        public void DeleteProduct(ulong id)
+        {
+            Product? product = GetProduct(id);
+            if (product != null)
+            {
+                Products.Remove(product);
+                SaveChanges();
+            }
+            else
+                throw new ProductNotFoundException();
+        }
+    }
+}
---
+++ CCLibrary/Data/ProfileContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using CCLibrary.User;
+using CCLibrary.Exceptions;
+
+namespace CCLibrary.Data
+{
+    public class ProfileContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(Data.Database._dbSource);
+
+        /// <summary>
+        /// Create a new profile.
+        /// </summary>
+        /// <exception cref="ProfileAlreadyExistsException"></exception>
+        public Profile CreateProfile(string login, string password)
+        {
+            if (Profiles.Any(profile =>
+                profile._login.Equals(login, StringComparison.OrdinalIgnoreCase)))
+                    throw new ProfileAlreadyExistsException();
+
+            Profile newProfile = new(login, password);
+            Profiles.Add(newProfile);
+            SaveChanges();
+            return newProfile;
+        }
+
+        /// <summary>
+        /// Get profile if it exists and the password is correct.
+        /// </summary>
+        /// <exception cref="ProfiletNotFoundException"></exception>
+        /// <exception cref="WrongPasswordException"></exception>
+        public Profile GetProfile(string login, string password)
+        {
+            Profile? profile = Profiles.FirstOrDefault(profile =>
+                profile._login.Equals(login, StringComparison.OrdinalIgnoreCase))
+                    ?? throw new ProfiletNotFoundException();
+
+            if (!profile.CheckPassword(password))
+                throw new WrongPasswordException();
+
+            return profile;
+        }
+    }
+}
