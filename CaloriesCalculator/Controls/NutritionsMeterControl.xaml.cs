acd9ccc72e900ee6ff25f86f41e017410b59c872
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-04-04 19:41:38 +0300

    Nutritions control

---
+++ CaloriesCalculator/Controls/NutritionsMeterControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CCLibrary.Products;
+
+namespace CaloriesCalculator.Controls
+{
+    public partial class NutritionsMeterControl : UserControl
+    {
+        public NutritionsMeterControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty NutritionProperty = 
+            DependencyProperty.Register(nameof(Nutrition), typeof(Nutrition), typeof(NutritionsMeterControl),
+                new PropertyMetadata(new Nutrition()));
+
+        public Nutrition Nutrition
+        {
+            get => (Nutrition)GetValue(NutritionProperty);
+            set => SetValue(NutritionProperty, value);
+        }
+    }
+}
