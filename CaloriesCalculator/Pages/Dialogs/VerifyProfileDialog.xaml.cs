1bee2a40594c3d7ba0aea7435a4dda260f477f11
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-04-05 14:18:21 +0300

    Register profile and reset password features

---
+++ CaloriesCalculator/Pages/Dialogs/VerifyProfileDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CaloriesCalculator.Pages.Dialogs
+{
+    public partial class VerifyProfileDialog : Window
+    {
+        public string Password { get; set; } = string.Empty;
+        public string SecretWord { get; set; } = string.Empty;
+
+        public VerifyProfileDialog(string note = "")
+        {
+            InitializeComponent();
+            NoteTextBlock.Text = note;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+            => Password = ((PasswordBox)sender).Password;
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+            => DialogResult = true;
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+            => DialogResult = false;
+    }
+}
--- CaloriesCalculator/Pages/ProfilePage.xaml.cs
+++ CaloriesCalculator/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CCLibrary.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,111 @@ using System.Windows.Shapes;
 
 namespace CaloriesCalculator.Pages
 {
-    /// <summary>
-    /// Interaction logic for ProfilePage.xaml
-    /// </summary>
     public partial class ProfilePage : Page
     {
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.Database.Context.Profiles.Any())
+                MessageBox.Show("Схоже, що на цьому пристрої відсутні користувачі.\nВам доведеться створити нового, задавши логін, пароль та секретне слово, яке може знадобитися, якщо ви забудете пароль.",
+                    "Відсутні користувачі", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private bool CheckLoginFields()
+        {
+            if (string.IsNullOrWhiteSpace(LoginTextBox.Text) || string.IsNullOrWhiteSpace(LoginPasswordBox.Password))
+            {
+                MessageBox.Show("Заповніть логін та пароль!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckLoginFields())
+                return;
+
+
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckLoginFields())
+                return;
+
+            Dialogs.VerifyProfileDialog verifyDialog = new("Повторіть ваш пароль, щоб переконатися в його достовірності, та вкажіть тайне слово, яке знадобиться при відновленні паролю.");
+            bool? result = verifyDialog.ShowDialog();
+
+            if (result != true)
+                return;
+
+            if (!LoginPasswordBox.Password.Equals(verifyDialog.Password, StringComparison.Ordinal))
+            {
+                MessageBox.Show("Паролі не співпадають!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(verifyDialog.SecretWord))
+            {
+                MessageBox.Show("Обов'язково вкажіть тайне слово!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                App.Database.Context.CreateProfile(LoginTextBox.Text, LoginPasswordBox.Password, verifyDialog.SecretWord);
+                MessageBox.Show("Ви створили профіль користувача.\nТепер ви можете використоати цей логін та пароль для авторизації.\nЯкщо ви забули пароль, використайте ваше тайне слово для його відновлення.",
+                    "Успіх", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (ProfileAlreadyExistsException ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ResetPasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginTextBox.Text))
+            {
+                MessageBox.Show("Введіть логін, за яким буде відбуватися відновлення!",
+                    "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Dialogs.VerifyProfileDialog verifyDialog = new("Введіть новий пароль та тайне слово для вашого профілю.");
+            bool? result = verifyDialog.ShowDialog();
+
+            if (result != true)
+                return;
+
+            if (string.IsNullOrWhiteSpace(verifyDialog.Password))
+            {
+                MessageBox.Show("Ви не вказали новий пароль!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(verifyDialog.SecretWord))
+            {
+                MessageBox.Show("Ви не вказали тайне слово!", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                App.Database.Context.ResetPassword(LoginTextBox.Text, verifyDialog.SecretWord, verifyDialog.Password);
+                MessageBox.Show("Ви встановили новий пароль!\nВикористайте його для авторизації.",
+                    "Успіх", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                if (ex is ProfiletNotFoundException || ex is WrongSecretWordException)
+                    MessageBox.Show(ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                    MessageBox.Show("Невідома помилка.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
